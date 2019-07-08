2b8eea54ca8c7f8daa7c3a4afe290268b7b00a6c
leadunkel <52069786+leadunkel@users.noreply.github.com>
2019-07-08 21:51:15 +0200

    Update Tetrisfinal.py

--- Tetrisfinal.py
+++ Tetrisfinal.py
@@ -5,7 +5,7 @@ import pygame, sys
 import gamemanager
 
 
-config = {						#Rahmenbedingungen für Tetris
+config = {						"""Rahmenbedingungen für Tetris"""
 	'cell_size':	20,
 	'cols':		13,
 	'rows':		20,
@@ -13,7 +13,7 @@ config = {						#Rahmenbedingungen für Tetris
 	'maxfps':	120
 }
 
-farben = [						#Farben für Steine und Hintergrund
+farben = [						"""Farben für Steine und Hintergrund"""
 (0,   0,   0),
 (247, 255,   0),
 (42, 255, 56),
@@ -24,7 +24,7 @@ farben = [						#Farben für Steine und Hintergrund
 (0,   220, 220)
 ]
 
-tetris_shapes = [					#Formen der Steine
+tetris_shapes = [					"""Formen der Steine"""
 	[[0, 0, 5],
 	 [5, 5, 5]],
 
@@ -51,7 +51,7 @@ def drehen(shape):
 			for y in range(len(shape)) ]
 		for x in range(len(shape[0]) - 1, -1, -1) ]
 
-def collision(board, shape, offset):			#steine stapeln
+def collision(board, shape, offset):			"""steine stapeln"""
 	off_x, off_y = offset
 	for cy, row in enumerate(shape):
 		for cx, cell in enumerate(row):
@@ -62,7 +62,7 @@ def collision(board, shape, offset):			#steine stapeln
 				return True
 	return False
 
-def remove_zeile(board, row):				#wenn eine Reihe vollständig ist, wird sie entfernt
+def remove_zeile(board, row):				"""wenn eine Reihe vollständig ist, wird sie entfernt"""
 	del board[row]
 	return [[0 for i in range(config['cols'])]] + board
 
@@ -93,7 +93,7 @@ class TetrisApp(object):
 
 		self.init_game()
 
-	def neuer_Stein(self):					#neue steine generieren
+	def neuer_Stein(self):					"""neue steine generieren"""
 		self.stone = tetris_shapes[rand(len(tetris_shapes))]
 		self.stone_x = int(config['cols'] / 2 - len(self.stone[0])/2)
 		self.stone_y = 0
@@ -154,7 +154,7 @@ class TetrisApp(object):
 		pygame.display.update()
 		gamemanager.main()
 
-	def fastdrop(self):			#Stein schneller als normal fallen lassen
+	def fastdrop(self):			"""Stein schneller als normal fallen lassen"""
 		if not self.gameover and not self.paused:
 			self.stone_y += 1
 			if collision(self.board,
@@ -194,7 +194,7 @@ class TetrisApp(object):
 		self.screen = pygame.display.set_mode((self.width, self.height))
 		pygame.display.update()
 
-		Steuerung = {				#die steuerung
+		Steuerung = {				"""die steuerung"""
 			'ESCAPE':	self.beenden,
 			'LEFT':		lambda:self.bewegen(-1),
 			'RIGHT':	lambda:self.bewegen(+1),
@@ -223,7 +223,7 @@ class TetrisApp(object):
 					                  self.stone_y))
 			pygame.display.update()
 
-			for event in pygame.event.get():		#die interaktionsmoeglichkeiten die der spieler hat
+			for event in pygame.event.get():		"""die interaktionsmoeglichkeiten die der spieler hat"""
 				if event.type == pygame.USEREVENT+1:
 					self.fastdrop()
 				elif event.type == pygame.QUIT:
